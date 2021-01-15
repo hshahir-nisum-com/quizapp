5cdb85a279186c202cbd066c23c0aaac189d5517
Hafiz Shahir Zain <hshahir@nisum.com>
2021-01-15 11:01:13 +0500

    annonymous work has done

--- src/component/testList/testList.js
+++ src/component/testList/testList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import "./testList.css";
 import data from "./subject.json";
 import { useHistory } from "react-router-dom";
